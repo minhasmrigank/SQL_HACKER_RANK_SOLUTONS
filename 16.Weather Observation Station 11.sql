d9e25acb68c58fdf930d030481a2670ad38690ca
Osiris <36435086+minhasmrigank@users.noreply.github.com>
2021-07-21 12:42:15 +0530

    Update 16.Weather Observation Station 11.sql

--- 16.Weather Observation Station 11.sql
+++ 16.Weather Observation Station 11.sql
@@ -2,3 +2,7 @@
 SELECT DISTINCT city FROM station WHERE NOT (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%')
 UNION
 SELECT DISTINCT city FROM station WHERE NOT ( city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' OR city LIKE '%o' OR city LIKE '%u');
+
+/*Another simple method using OR*/
+SELECT DISTINCT city FROM station WHERE NOT (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%')
+OR NOT ( city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' OR city LIKE '%o' OR city LIKE '%u');
