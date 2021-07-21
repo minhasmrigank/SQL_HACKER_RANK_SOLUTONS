6741c4cca994e8b99e5cf9a52bb3d73d61502300
Osiris <36435086+minhasmrigank@users.noreply.github.com>
2021-07-21 12:01:52 +0530

    Add files via upload

---
+++ 11.Weather Observation Station 6.sql
@@ -0,0 +1,3 @@
+/*Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
+ Your result cannot contain duplicates.*/
+SELECT city FROM station WHERE (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%');
