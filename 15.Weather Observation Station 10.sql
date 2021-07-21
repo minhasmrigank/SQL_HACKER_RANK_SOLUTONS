592f7f12cf71c9fec90c7e1cd9c7bae47cff2f78
Osiris <36435086+minhasmrigank@users.noreply.github.com>
2021-07-21 12:19:29 +0530

    Add files via upload

---
+++ 15.Weather Observation Station 10.sql
@@ -0,0 +1,2 @@
+/*Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.*/
+SELECT Distinct  city FROM station WHERE not( city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' OR city LIKE '%o' OR city LIKE '%u');
